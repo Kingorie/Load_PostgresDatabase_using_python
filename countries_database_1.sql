23b7c14afe1b8f5540daf6b700ecad10628d5b7b
Kingorie <104713286+Kingorie@users.noreply.github.com>
2024-01-27 16:17:36 +0300

    changed files and code

---
+++ countries_database_1.sql
@@ -0,0 +1,13 @@
+select * from public."product" limit 3;
+
+select * from public."region" limit 3;
+
+select * from public."reseller" limit 3;
+
+select * from public."sales" limit 3;
+
+select * from public."sales_person" limit 3;
+
+select * from public."salesperson_region" limit 3;
+
+select * from public."targets" limit 3;
