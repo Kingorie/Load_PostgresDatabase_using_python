4e8332a51ce3e72c3ec84511c69aeafa0523868b
Kingorie <104713286+Kingorie@users.noreply.github.com>
2024-01-28 10:26:56 +0300

    updated code and files

--- countries_database_1.sql
+++ countries_database_1.sql
@@ -1,13 +1,13 @@
-select * from public."product" limit 3;
+select * from public.cost_of_living_index_by_country_2020 limit 3;
 
-select * from public."region" limit 3;
+select * from public.coutries_age_structure limit 3;
 
-select * from public."reseller" limit 3;
+select * from public.crime_index_by_countries_2020 limit 3;
 
-select * from public."sales" limit 3;
+select * from public.health_care_index_by_countries_2020 limit 3
 
-select * from public."sales_person" limit 3;
+select * from public.properties_price_index_by_countries_2020 limit 3
 
-select * from public."salesperson_region" limit 3;
+select * from public.pupulation_density_by_countries limit 3
 
-select * from public."targets" limit 3;
+select * from public.quality_of_life_index_by_countries_2020 limit 3
